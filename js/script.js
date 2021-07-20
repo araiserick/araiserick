c12887706e548e382d6d34fad9ef4f109d28ecf0
Erick <arais.erick@yandex.ru>
2021-07-20 15:26:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 8;
+
+number = 10;
+console.log (number);
+//alert('hello');
+
+//const result = confirm ("Are you here?");
+//console.log (result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+const answer = [];
+answer[0] = prompt ("как ваше ничего?","");
+answer[1] = prompt ("что хотите?","");
+answer[2] = prompt ("что дадите?","");
+
+document.write(answer);
