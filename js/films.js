1127eef4b4461ae05e10aafc352b2a35f028adaa
Erick <arais.erick@yandex.ru>
2021-07-20 17:54:21 +0300

    add new films.js

---
+++ js/films.js
@@ -0,0 +1,18 @@
+let numberOfFilms =  +prompt ("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let nameFilms = prompt ('Один из последних просмотренных фильмов', ''),
+    nameFilms1 = prompt('Один из последних просмотренных фильмов', '');
+
+let estimationFilms = prompt('На сколько оцените его?'),
+    estimationFilms1= prompt('На сколько оцените его?');
+
+personalMovieDB.movie[nameFilms] = estimationFilms;
+personalMovieDB.movie[nameFilms1] = estimationFilms1;
+console.log(personalMovieDB);
