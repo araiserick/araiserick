d7e67f6354219c52e95b4027aa514c3e4a63c2ec
Erick <arais.erick@yandex.ru>
2021-07-21 10:19:41 +0300

    add new while.js

---
+++ js/if.js
@@ -0,0 +1,19 @@
+"use strict";
+
+if (4 == 9) {
+    console.log("ОК!");
+}
+else{
+    console.log("ERROR");
+}
+
+const num = 50;
+
+if ( num < 49) {
+    console.log ('ERROR');
+} else if (num > 100) {
+    console.log('Много');
+}
+else {
+    console.log('ОК!');
+}
